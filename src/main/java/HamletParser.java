38ed434657a9c61a4e2b3818573e5ea49879700c
sylram <sylram7@gmail.com>
2019-03-16 13:35:22 -0400

    Hamlet is Leon now

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -9,31 +11,104 @@ public class HamletParser {
 
     private String hamletData;
 
-    public HamletParser(){
+
+    public HamletParser() {
         this.hamletData = loadFile();
     }
-
-    private String loadFile(){
+    
+    private String loadFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
 
-        try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
-
             scanner.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return result.toString();
     }
 
-    public String getHamletData(){
+    public String getHamletData() {
+
         return hamletData;
     }
 
+    public String getNewHamletData(){
+        String retrieved = getHamletData();
+        String hamletReplaced = replaceData(retrieved,"Leon",findHamletMatcher(retrieved));
+        return replaceData(hamletReplaced,"Tariq",findHoratioMatcher(hamletReplaced));
+    }
+
+    public static String replaceData(String given, String replacement, Matcher matcher) {
+        StringBuffer replaced = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(replaced, replacement);
+        }
+        matcher.appendTail(replaced);
+        return replaced.toString();
+    }
+
+    public static Matcher findHoratioMatcher(String given) {
+        String patternString = "Horatio";
+        Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(given);
+        return matcher;
+    }
+
+    public static boolean findHamlet(String given) {
+        return findHamletMatcher(given).find();
+    }
+
+    public static boolean findHoracio(String given) {
+        return findHoratioMatcher(given).find();
+    }
+
+
+    public static Matcher findHamletMatcher(String given) {
+        String patternString = "Hamlet";
+//        String patternString= "Hamlet";
+        Pattern pattern = Pattern.compile(patternString, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(given);
+//        Pattern.matches(pattern, given);
+        return matcher;
+    }
+
+    public static void main(String[] args) {
+        HamletParser hamletParser = new HamletParser();
+        String retrieve = hamletParser.getNewHamletData();
+        System.out.println(retrieve);
+    }
+
 }
+
+//
+//
+//
+//
+//        String patternString = "((.*Hamlet.*) (.+?))";
+//        Pattern pattern = Pattern.compile(patternString);
+//        Matcher matcher = pattern.matcher(given);
+//        String replaced = matcher.replaceAll("Leon");
+//
+//
+
+
+
+
+
+//    public static String getHamlet(String str){
+//        String patternString = ".*Hamlet.*";
+//        Pattern pattern = Pattern.compile(patternString,Pattern.CASE_INSENSITIVE);
+//        Matcher matcher = pattern.matcher(str);
+//        String pattern2 = pattern.pattern();
+//
+//    }
+
+
+
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,7 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HamletParserTest {
     private String hamletText;
@@ -15,17 +17,89 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        //given
+        String given ="Enter Hamlet- and MARCELLUS and Hamlet";
+        String expected = "Enter Leon- and MARCELLUS and Leon";
+
+        //When
+        Matcher matcher = HamletParser.findHamletMatcher(given);
+        String actual = HamletParser.replaceData(given,"Leon",matcher);
+
+        //Then
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //given
+        String given ="Enter Horatio and MARCELLUS and Horatio";
+        String expected = "Enter Tariq and MARCELLUS and Tariq";
+
+        //When
+        Matcher matcher = HamletParser.findHoratioMatcher(given);
+        String actual = HamletParser.replaceData(given,"Tariq",matcher);
+
+        //Then
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void testFindHoratio() {
+        //given
+        String given ="Enter HORATIO and MARCELLUS";
+
+        //When
+        boolean actual = HamletParser.findHoracio(given);
+
+        //Then
+        Assert.assertTrue(actual);
     }
 
     @Test
     public void testFindHamlet() {
+        //given
+        String given ="The Tragedy of Hamlet .";
+
+        //When
+        boolean actual = HamletParser.findHamlet(given);
+
+        //Then
+        Assert.assertTrue(actual);
+    }
+    @Test
+    public void testReplaceData() {
+        //given
+        HamletParser hamletParser = new HamletParser();
+
+
+        //When
+        String textChanged = hamletParser.getNewHamletData();
+        boolean actual = HamletParser.findHamlet(textChanged);
+        boolean actual2 = HamletParser.findHoracio(textChanged);
+
+        //Then
+        Assert.assertFalse(actual);
+        Assert.assertFalse(actual2);
+    }
+    @Test
+    public void testReplaceData2() {
+        //given
+        HamletParser hamletParser = new HamletParser();
+        String textChanged = hamletParser.getNewHamletData();
+        Pattern pattern = Pattern.compile("Leon",Pattern.CASE_INSENSITIVE);
+        Pattern pattern1 = Pattern.compile("Tariq",Pattern.CASE_INSENSITIVE);
+
+
+        //When
+        Matcher matcher = pattern.matcher(textChanged);
+        Matcher matcher1 = pattern1.matcher(textChanged);
+
+        boolean actual = matcher.find();
+        boolean actual2 = matcher1.find();
+
+
+        //Then
+        Assert.assertTrue(actual);
+        Assert.assertTrue(actual2);
     }
 }
